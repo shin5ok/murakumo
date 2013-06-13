b0f21b804e162b5c67f81ea97032ecc8480c78b0
shin5ok <shin5ok@55mp.com>
2013-06-13 17:46:35 +0900

    modified murakumo.conf and modules to add check_storage_size parameter

--- lib/Murakumo/CLI/Storage/Select.pm
+++ lib/Murakumo/CLI/Storage/Select.pm
@@ -11,16 +11,20 @@ use Murakumo::CLI::Utils;
 use Murakumo::CLI::DB;
 use base qw( Murakumo::CLI::DB );
 
-# my $config = Murakumo::CLI::Utils->config;
+my $config = Murakumo::CLI::Utils->config;
 
 sub select {
   my ($self, $size, $query_args) = @_;
   my $resultset = $self->schema->resultset('Storage');
 
   $query_args //= +{};
-  $query_args->{avail_size} = +{ '>' => $size };
 
-  warn Dumper $query_args;
+  {
+    no strict 'refs';
+    if ($size and $config->{check_storage_size}) {
+      $query_args->{avail_size} = +{ '>' => $size };
+    }
+  }
 
   my @rs = $resultset->search($query_args, { order_by => { -desc => [ 'priority' ] } });
   @rs = sort { $a->iowait <=> $b->iowait } @rs;
